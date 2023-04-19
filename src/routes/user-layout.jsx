1a85b8c98331b55ddf2f32b804dbb04707a5c8d7
Anurag Biswal <anugbswl@gmail.com>
2023-04-19 15:02:26 +0530

    created user's profile page

---
+++ src/routes/user-layout.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { BiBell, BiArrowBack, BiLogOut } from "react-icons/bi";
+import { UserAuth } from "../contexts/AuthContext";
+export default function UserLayout() {
+  return (
+    <div>
+      <ProfileNav />
+      <Outlet />
+    </div>
+  );
+}
+
+function ProfileNav() {
+  const { currentUser, logOut } = UserAuth();
+  return (
+    <div className="bg-cyan-800">
+      <div className="navbar   sm:w-9/12 mx-auto">
+        <div className="navbar-start space-x-2">
+          <div className="tooltip tooltip-right" data-tip="home">
+            <Link to="/">
+              <button className=" btn btn-ghost btn-circle text-xl btn-sm md:btn-md">
+                <BiArrowBack className="text-xl" />
+              </button>
+            </Link>
+          </div>
+          <h2 className=" normal-case text-xl select-none">EventHive</h2>
+        </div>
+        <div className="navbar-center"></div>
+        <div className="navbar-end space-x-2">
+          <div
+            className="tooltip tooltip-bottom"
+            data-tip="participate in events"
+          >
+            <Link to={"#"}>
+              <button className="btn btn-sm md:btn-md btn-secondary normal-case">
+                Participate
+              </button>
+            </Link>
+          </div>
+          {currentUser && (
+            <>
+              <div className="tooltip tooltip-bottom" data-tip="Notifications">
+                <button className="btn btn-ghost btn-circle btn-sm md:btn-md">
+                  <div className="indicator">
+                    <BiBell className="text-xl" />
+                    <span className="badge badge-xs badge-primary indicator-item"></span>
+                  </div>
+                </button>
+              </div>
+              <div className="tooltip tooltip-bottom" data-tip="sign out">
+                <button
+                  className="btn btn-ghost btn-circle text-xl btn-sm md:btn-md"
+                  onClick={() => logOut()}
+                >
+                  <BiLogOut className="text-xl" />
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/routes/user-profile.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { UserAuth } from "../contexts/AuthContext";
+import { BiMap, BiBookBookmark } from "react-icons/bi";
+
+export default function UserProfile() {
+  const { currentUser } = UserAuth();
+  return (
+    <div className="lg:w-9/12 mx-auto md:flex mt-6 px-2 space-x-2">
+      <div className="px-2 py-4  max-w-xs mx-auto bg-slate-800 bg-opacity-25">
+        <div className="w-60 mask mask-squircle mx-auto">
+          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+        <div className="text-center my-2 py-2  max-w-xs mx-auto space-y-1">
+          <h3 className="text-2xl font-semibold">
+            {currentUser.displayName || "Current User"}
+          </h3>
+          {/* <h5>{ currentUser.email || 'user\'s email'}</h5> */}
+          <h5 className="text-lg font-light font-mono">username</h5>
+          <button className="btn btn-outline btn-sm normal-case w-full max-w-[15rem] ">
+            Edit Profile
+          </button>
+
+          <div className="mx-8 md:mx-0 space-y-1 py-2">
+            <div className="flex space-x-2 items-center">
+              <BiMap />
+              <span className="font-sans">
+                {currentUser.location || "User's location"}
+              </span>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <BiBookBookmark />
+              <span className="font-sans">
+                {currentUser.college || "User's college"}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-2 py-4 flex-1 bg-slate-800 bg-opacity-25">
+        <div className="min-h-[5rem]">
+          <div className="border-b border-slate-700 pb-1">
+            <div
+              className="tooltip tooltip-right"
+              data-tip="recent participated events"
+            >
+              <span className="text-xl">Recent Events</span>
+            </div>
+          </div>
+        </div>
+        <div className="min-h-[5rem]">
+          <div className="border-b border-slate-700 pb-1 min-h-">
+            <div
+              className="tooltip tooltip-right text-xl"
+              data-tip="past participated events"
+            >
+              Past Events
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
