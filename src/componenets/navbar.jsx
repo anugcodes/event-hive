5674edd9d9291b8f257339d930ce27f79525aede
Anurag Biswal <anugbswl@gmail.com>
2023-04-19 15:01:17 +0530

    added login and sign in buttons

--- src/componenets/navbar.jsx
+++ src/componenets/navbar.jsx
@@ -1,46 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BiBell, BiMenu, BiSearch } from "react-icons/bi";
+import { UserAuth } from "../contexts/AuthContext";
+
 export function PageNav() {
+  const { logOut, currentUser } = UserAuth();
+  const navigate = useNavigate();
+  async function handleLogOut() {
+    await logOut();
+    console.log("logged out");
+    navigate("/");
+  }
+
   return (
     <div className="navbar bg-base-100 sticky top-0 sm:w-9/12 mx-auto">
       <div className="navbar-start">
         <Link to={"/"} className=" normal-case text-xl">
-         EventHive 
+          EventHive
         </Link>
       </div>
       <div className="navbar-center"></div>
       <div className="navbar-end space-x-2">
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <BiBell className="text-2xl font-bold" />
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
-        <Link to={"#events"} className="btn btn-ghost normal-case">Events</Link>
-        <label htmlFor="loginModal" className="btn btn-ghost normal-case">Log in</label>
-        <label htmlFor="RegisterModal" className="btn btn-primary normal-case">Get Started</label>
-        
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src="https://cdn-icons-png.flaticon.com/512/5556/5556468.png" />
+        <Link to={"#events"} className="btn btn-ghost normal-case">
+          Events
+        </Link>
+        {!currentUser && (
+          <>
+            <label htmlFor="loginModal" className="btn btn-ghost normal-case">
+              Log in
+            </label>
+            <label
+              htmlFor="RegisterModal"
+              className="btn btn-primary normal-case"
+            >
+              Get Started
+            </label>
+          </>
+        )}
+        {currentUser && (
+          <>
+            <div className="tooltip tooltip-bottom" data-tip="Notifications">
+              <button className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <BiBell className="text-2xl font-bold" />
+                  <span className="badge badge-xs badge-primary indicator-item"></span>
+                </div>
+              </button>
             </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">Profile</a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+            <div className="tooltip tooltip-right" data-tip="Profile">
+              <Link to={"/user"} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </Link>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
