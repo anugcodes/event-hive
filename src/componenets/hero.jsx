c3e869702d94ecb530df38977125a60620334d35
Anurag Biswal <anugbswl@gmail.com>
2023-04-19 15:00:59 +0530

    added link for register

--- src/componenets/hero.jsx
+++ src/componenets/hero.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserAuth } from "../contexts/AuthContext";
 
 export default function PageHero() {
+    const {currentUser} = UserAuth();
   return (
-    <div className="hero min-h-[60vh] w-9/12 mx-auto">
+    <div className="hero min-h-[60vh] sm:w-9/12 mx-auto">
       <div className="hero-content flex-col lg:flex-row">
         <img
           src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="max-w-sm rounded-lg "
         />
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
@@ -16,7 +18,14 @@ export default function PageHero() {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <Link to='/auth/login' className="btn btn-primary">Get Started</Link>
+          {!currentUser && (
+            <label
+              htmlFor="RegisterModal"
+              className="btn btn-primary normal-case"
+            >
+              Get Started
+            </label>
+          )}
         </div>
       </div>
     </div>
