b20147cb71c9eff57b9bdd80bf19f6aa3e6bd4df
Anurag Biswal <anugbswl@gmail.com>
2023-04-20 21:33:10 +0530

    updated hero section

--- src/componenets/hero.jsx
+++ src/componenets/hero.jsx
@@ -3,31 +3,31 @@ import { Link } from "react-router-dom";
 import { UserAuth } from "../contexts/AuthContext";
 
 export default function PageHero() {
-    const {currentUser} = UserAuth();
+  const { currentUser } = UserAuth();
   return (
-    <div className="hero min-h-[60vh] sm:w-9/12 mx-auto">
-      <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-          className="max-w-sm rounded-lg "
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+    <section className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1602810319428-019690571b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+      <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
+        <div className="text-center ltr:sm:text-left rtl:sm:text-right">
+          <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
+            Latest Shirts
+          </h2>
+
+          <p className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            officia corporis quasi doloribus iure architecto quae voluptatum
+            beatae excepturi dolores.
           </p>
-          {!currentUser && (
-            <label
-              htmlFor="RegisterModal"
-              className="btn btn-primary normal-case"
+
+          <div className="mt-4 sm:mt-8">
+            <a
+              href="#"
+              className="inline-block rounded-full bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
             >
-              Get Started
-            </label>
-          )}
+              Get Yours Today
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
