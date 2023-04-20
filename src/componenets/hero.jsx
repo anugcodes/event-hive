2d0be05fa56325c88cdd33e4dbf8349aa696a28c
Anurag Biswal <anugbswl@gmail.com>
2023-04-21 03:31:02 +0530

    hero design final

--- src/componenets/hero.jsx
+++ src/componenets/hero.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { UserAuth } from "../contexts/AuthContext";
-
+import "../styles/hero.css";
 export default function PageHero() {
   const { currentUser } = UserAuth();
   return (
-    <section className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1602810319428-019690571b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
-      <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
+    <section className="overflow-hidden">
+      <div className=" p-12 md:p-16 lg:px-16 lg:py-28">
+        <div className="blur"></div>
         <div className="text-center ltr:sm:text-left rtl:sm:text-right">
-          <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
-            Latest Shirts
+          <h2 className=" hero-heading text-4xl  sm:text-4xl md:text-6xl">
+            Join the Community of Tech and Non-Tech Enthusiasts Where Your
+            Passion Meets Opportunities
           </h2>
 
-          <p className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-            officia corporis quasi doloribus iure architecto quae voluptatum
-            beatae excepturi dolores.
+          <p className="max-w-2xl mx-auto my-6   text-center">
+            Explore events, fests, conferences from different colleges across
+            the nation to learn, showcase skills, build communities, interact
+            with like-minded people, display their achievements and pursue their
+            passions.
           </p>
 
           <div className="mt-4 sm:mt-8">
-            <a
-              href="#"
+            <Link
+              to="/auth/register"
               className="inline-block rounded-full bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
             >
-              Get Yours Today
-            </a>
+              Get Started
+            </Link>
           </div>
         </div>
       </div>
